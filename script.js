93bf19d2b4b3c67296c6f185d3c087217cd20dab
Manuel Valencia <valencialejo22@gmail.com>
2021-05-17 22:06:05 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+
+    if (principal<"0" || principal=="0"){
+        alert("Enter a positive number")
+        principal.focus()
+        return false
+    }
+
+    var rate=document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
+
+    var interest=principal*years*rate/100;
+    var year=new Date().getFullYear()+parseInt(years);
+
+    var texto="If you deposit "+ principal+",<br>at an interest rate of "+rate+".<br>You will receive an amount of "+interest+",<br>in the year "+year;
+    document.getElementById("result").innerHTML=texto;
 }
-        
+
+function updateRate(){
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
